e8241f70b5de99d2fe163ca748155ff4935eabd2
RicciFlowing <benjamin@mathe-sellin.de>
2015-05-21 18:31:53 +0200

    Added tie-break to make the graph-collection accept corners and edges

--- graph.js
+++ graph.js
@@ -1,4 +1,29 @@
-var Corner = Backbone.Model.extend({});
-var Edge   = Backbone.Model.extend({});
-var Part   = Backbone.Model.extend({});
+var Corner = Backbone.Model.extend({
+  defaults:  {
+    type: 'corner'
+  }
+});
+var Edge   = Backbone.Model.extend({
+  defaults:  {
+    type: 'edge'
+  }
+});
+var Part   = Backbone.Model.extend({
+  model: function(attr,options){
+       switch(attr.type){
+           case 'corner':
+               return new Corner(attr,options);
+               break;
+           case 'edge':
+               return new Edge(attr,options);
+               break;
+       }
+   }
+});
+
 var Graph  = Backbone.Collection.extend({model: Part});
+
+var graph1 = new Graph();
+var p = new Corner({ x : 1 , y: 2});
+graph1.add(p);
+console.log(graph1.length);
