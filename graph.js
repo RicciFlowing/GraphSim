b1cca0f6b757e4e380f0189c2a97fbc1383b0783
RicciFlowing <benjamin@mathe-sellin.de>
2015-05-21 19:02:50 +0200

    Added rendering for corners

--- graph.js
+++ graph.js
@@ -1,13 +1,34 @@
+var canvas = oCanvas.create({
+	canvas: "#canvas"
+});
+
 var Corner = Backbone.Model.extend({
   defaults:  {
-    type: 'corner'
+    type: 'corner',
+    x: canvas.width/2,
+    y: canvas.height/2,
+  },
+  render: function(){
+    var circle = canvas.display.ellipse({
+      x: this.get('x'),
+      y: this.get('y'),
+      radius: 60,
+      fill: "#079"
+    });
+    canvas.addChild(circle);
   }
 });
+
+
 var Edge   = Backbone.Model.extend({
   defaults:  {
     type: 'edge'
+  },
+  render: function(){
+
   }
 });
+
 var Part   = Backbone.Model.extend({
   model: function(attr,options){
        switch(attr.type){
@@ -24,6 +45,31 @@ var Part   = Backbone.Model.extend({
 var Graph  = Backbone.Collection.extend({model: Part});
 
 var graph1 = new Graph();
-var p = new Corner({ x : 1 , y: 2});
+var p = new Corner({ x : 500 , y: 20});
+p.render();
 graph1.add(p);
-console.log(graph1.length);
+
+
+
+var button = canvas.display.rectangle({
+	x: canvas.width / 2,
+	y: canvas.width / 5,
+	origin: { x: "center", y: "center" },
+	width: 300,
+	height: 40,
+	fill: "#079",
+	stroke: "10px #079",
+	join: "round"
+});
+var buttonText = canvas.display.text({
+	x: 0,
+	y: 0,
+	origin: { x: "center", y: "center" },
+	align: "center",
+	font: "bold 25px sans-serif",
+	text: "Toggle Rotation",
+	fill: "#fff"
+});
+button.addChild(buttonText);
+
+canvas.addChild(button);
