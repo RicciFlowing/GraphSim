33ec01b391294647c2ff178b5e016e4f19082f9f
RicciFlowing <benjamin@mathe-sellin.de>
2015-05-25 20:33:47 +0200

    Moved grapheditClass to an extra script file

--- graph.js
+++ graph.js
@@ -109,45 +109,6 @@ graph.on("add",render_part);
 graph.on("change:x",render_part);
 graph.on("change:y",render_part);
 
-
-
-function GraphEdit(graph_to_edit, target_canvas){
-	this.addCorner = function(){
-	  var p = new Corner({ x : target_canvas.mouse.x , y: target_canvas.mouse.y});
-		graph_to_edit.add(p);
-	};
-
-	this.addEdge = function(){
-		if(!this.corner_selected){
-			this.start_corner = graph_to_edit.find_nearest_corner(target_canvas.mouse.x,target_canvas.mouse.y);
-			this.corner_selected = true;
-		}
-		else {
-			this.end_corner = graph_to_edit.find_nearest_corner(target_canvas.mouse.x,target_canvas.mouse.y);
-			var edge = new Edge({start: this.start_corner, end: this.end_corner});
-			graph_to_edit.add(edge);
-			this.corner_selected = false;
-			this.start_corner = {};
-			this.end_corner = {};
-		}
-	};
-
-	this.set_corner_mode = function(){
-		target_canvas.unbind("click", this.addEdge);
-		target_canvas.bind("click", this.addCorner );
-	};
-
-	this.set_edge_mode = function(){
-		canvas.start_corner = {};
-		canvas.end_corner = {};
-		canvas.corner_selected = false;
-		target_canvas.unbind("click", this.addCorner );
-		target_canvas.bind("click", this.addEdge );
-	};
-
-
-};
-
 var menu = new GraphEdit(graph, canvas);
 menu.set_edge_mode();
 canvas.bind("keydown",		function() {
